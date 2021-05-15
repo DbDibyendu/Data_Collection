5a6737910e509fa8a1f7c1e06419e8a19e6d1cda
Dibyendu <dbdibyendu5@gmail.com>
2021-05-15 18:15:10 +0530

    edited

--- graph_app/urls.py
+++ graph_app/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path , include 
 from  . import views 
-from .views import CryptoAPI, Forecast
+from .views import CryptoAPI
 
 urlpatterns = [
     path('graph/' , CryptoAPI.as_view()),
-    path('graph2/' , Forecast.as_view()),
 ]
--- graph_app/views.py
+++ graph_app/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,10 +7,10 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.offline import plot
+import matplotlib.pyplot as plt
+import matplotlib  
+matplotlib.use('Agg')
 
-import base64
-import urllib
-import io
 from io import BytesIO
 from PIL import Image
 from django.shortcuts import render
@@ -31,8 +30,6 @@ from rest_framework.decorators import api_view
 from cryptocmd import CmcScraper
 from prophet.plot import plot_plotly, plot_components_plotly
 from fbprophet import Prophet
-import csv
-from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
@@ -63,31 +60,3 @@ class CryptoAPI(APIView):
         context = {'graph1': graph1}
 
         return Response(context)
-
-class Forecast(APIView):
-    serializer_class = InteractiveSerializer
-    
-    def post(self, request, format=None):
-        coin = request.data.get('crypto')
-        scraper = CmcScraper(coin)
-        headers, data = scraper.get_data()
-        xrp_json_data = scraper.get_data("json")
-        scraper.export("csv", name="btc_all_time")
-        df_BTC = scraper.get_dataframe()
-        df_BTC_proph = df_BTC[['Date', 'Close']] 
-        df_BTC_proph = df_BTC.rename(columns={'Date':'ds', 'Close':'y'}) 
-        m=Prophet()
-        m.fit(df_BTC_proph)
-        future = m.make_future_dataframe(periods=365)
-        forecast = m.predict(future)
-        fig1 = plot_plotly(m, forecast,xlabel='Date', ylabel='Close')
-        fig1.update_layout(autosize=True,width=1500, height=800)
-        graph1 = plot(fig1, output_type='div')
-
-        fig2 = plot_components_plotly(m, forecast)
-        fig2.update_layout(autosize=True,width=1500, height=800)
-        graph2 = plot(fig2, output_type='div')
-
-        context={'graph1':graph1, 'graph2':graph2}
-        return Response(context)
-        # return render(request,'index2.html',context)
