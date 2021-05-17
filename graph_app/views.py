fc542b5739551121e9b6c2eb20a9fdbaaf82690c
Dibyendu <dbdibyendu5@gmail.com>
2021-05-17 21:34:27 +0530

    edited

--- graph_app/views.py
+++ graph_app/views.py
@@ -43,7 +43,7 @@ class CryptoAPI(APIView):
     
     def post(self, request, format=None):
         coin = request.data.get('crypto')
-        scraper = CmcScraper(coin)
+        scraper = CmcScraper(coin)                                                          
         headers, data = scraper.get_data()
         xrp_json_data = scraper.get_data("json")
         # scraper.export("csv", name="btc_all_time")
@@ -90,6 +90,71 @@ class StocksCompanyAPI(APIView):
         fig.update_layout(template='plotly_dark')
         
         graph1 = plot(fig, output_type='div')
-        context = {'graph1': graph1}
+
+
+
+        # Passing the Data here
+        stock = yf.Ticker(input_stock_ticker)
+        hist = stock.history(period='max', interval='1d')
+        info = pd.DataFrame.from_dict(stock.info)
+
+        stock.financials.to_csv(r'financials.csv')
+        financials = pd.read_csv(r'financials.csv')
+
+        new = ""
+        j = 0
+        for col in financials.columns:
+            for i in col:
+                if i != '-':
+                    new += i
+                else:
+                    new+='/'
+
+            financials.columns.values[j] = new
+            new = ""
+            j=j+1
+ 
+        financials = financials.fillna('')
+
+
+        stock.cashflow.to_csv(r'cashflow.csv')
+        cashflow = pd.read_csv(r'cashflow.csv')
+
+        new = ""
+        j = 0
+        for col in cashflow.columns:
+            for i in col:
+                if i != '-':
+                    new += i
+                else:
+                    new+='/'
+
+            cashflow.columns.values[j] = new
+            new = ""
+            j=j+1
+
+        cashflow = cashflow.fillna('')
+
+        stock.balance_sheet.to_csv(r'balance_sheet.csv')
+        balance_sheet = pd.read_csv(r'balance_sheet.csv')
+
+        new = ""
+        j = 0
+        for col in balance_sheet.columns:
+            for i in col:
+                if i != '-':
+                    new += i
+                else:
+                    new+='/'
+
+            balance_sheet.columns.values[j] = new
+            new = ""
+            j=j+1
+
+        balance_sheet = balance_sheet.fillna('')
+
+        context = {'graph1': graph1,'instiutional holders': stock.institutional_holders,
+                   "major holders": stock.major_holders, "financials":financials, "cashflow":cashflow,"balance_sheet":balance_sheet}
+        
         # return render(request,"index2.html",context)
         return Response(context)
